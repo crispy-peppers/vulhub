76778d6b70c18ae61ad49860a20a83149c0b3a8f
crispy-peppers <23626307+crispy-peppers@users.noreply.github.com>
2019-12-11 16:19:00 +0530

    Update patch.sh

--- xss_domainmod_1191518/patch.sh
+++ xss_domainmod_1191518/patch.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-a2enmod rewrite
-cd /etc/apache2/sites-available/
-mv /etc/apache2/sites-available/000-default.conf /tmp
-wget https://raw.githubusercontent.com/crispy-peppers/vulhub/master/xss_domainmod_1191518/000-default.conf
-cd -
-service apache2 restart
-echo \$web_root = \$_SERVER[ \'HTTP_X_ORIGINAL_URI\' ]\; >> /var/www/html/domain/_includes/config.inc.php
-cd /var/www/html/
-wget https://raw.githubusercontent.com/crispy-peppers/vulhub/master/xss_domainmod_1191518/.htaccess
+cd /var/www/html/domain/_includes
+mv config.inc.php /tmp
+wget https://raw.githubusercontent.com/crispy-peppers/vulhub/master/xss_domainmod_1191518/_includes/config.inc.php
